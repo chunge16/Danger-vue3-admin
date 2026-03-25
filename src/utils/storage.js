8fb90156be65b15c65d7d72402c98b2437501efe
Jack shi <chunge1945@outlook.com>
2026-03-25 17:50:22 +0800

    refactor: split utils into domain modules

---
+++ src/utils/storage.js
@@ -0,0 +1,41 @@
+/**
+ * @description 获取localStorage
+ * @param {String} key Storage名称
+ * @returns {String}
+ */
+export function localGet(key) {
+  const value = window.localStorage.getItem(key);
+  if (value === null) return null;
+  try {
+    return JSON.parse(value);
+  } catch (error) {
+    return value;
+  }
+}
+
+/**
+ * @description 存储localStorage
+ * @param {String} key Storage名称
+ * @param {*} value Storage值
+ * @returns {void}
+ */
+export function localSet(key, value) {
+  window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+/**
+ * @description 清除localStorage
+ * @param {String} key Storage名称
+ * @returns {void}
+ */
+export function localRemove(key) {
+  window.localStorage.removeItem(key);
+}
+
+/**
+ * @description 清除所有localStorage
+ * @returns {void}
+ */
+export function localClear() {
+  window.localStorage.clear();
+}
